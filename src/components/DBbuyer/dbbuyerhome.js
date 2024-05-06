80d02a47c69bd85c2e81041653ca65fdcc7dd3d1
Kysier Ong Zheng Quan <kysier98@gmail.com>
2024-05-07 02:40:35 +0800

    Update dbbuyerhome.js
    - added buyer profile into buyer homepage

--- src/components/DBbuyer/dbbuyerhome.js
+++ src/components/DBbuyer/dbbuyerhome.js
@@ -4,9 +4,13 @@ import DashboardBuyer from './dbbuyer';
 const DBhome = () => {
     return (
 <div className="flex flex-col w-3/4 bg-gray-200 p-8 justify-start">
-<div className="flex gap-4 justify-center font-mono font-semibold">
-    Welcome, <span id='buyer'>Buyer</span> {/* Buyer's name using ID */}
-</div>
+    <h1 className="flex gap-4 justify-center font-mono font-semibold">
+        Welcome, <span id='buyer'>Buyer</span> {/* Buyer's name using ID */}
+    </h1>
+    {/* Buyer's profile add their profile in here */}
+    <p className="flex gap-4 justify-center">
+        Hi I am a buyer interested to buy around Serangoon
+    </p>
 </div>
     );
 };
