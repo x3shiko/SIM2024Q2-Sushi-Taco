75279be2a9058a959410c89bfd58426d013e05da
Kysier Ong Zheng Quan <kysier98@gmail.com>
2024-05-11 02:39:49 +0800

    Update to rrAgent.js
    +Completely change buyer review agent page to look similar to seller review agent page

--- src/components/DBbuyer/rrAgents.js
+++ src/components/DBbuyer/rrAgents.js
@@ -1,7 +1,6 @@
-import React,{useState} from 'react';
-import Modal from 'react-modal';
-import DashboardBuyer from './dbbuyer';
-import agent1 from "../../assets/Brian.jpeg";
+import React, { useState } from "react";
+import Modal from "react-modal";
+import DashboardBuyer from "./dbbuyer";
 
 const Rating = () => {
   const [rating, setRating] = useState(0); // state for rating
@@ -17,7 +16,7 @@ const Rating = () => {
         <button
           key={value}
           className={`${
-            value <= rating ? 'text-yellow-300' : 'text-gray-300'
+            value <= rating ? "text-yellow-300" : "text-gray-300"
           } focus:outline-none text-2xl`}
           onClick={() => handleClick(value)}
         >
@@ -29,97 +28,103 @@ const Rating = () => {
 };
 
 const RateReviewAgents = () => {
-  const [isOpen, setIsOpen] = useState(false); // state for modal
-  const [review, setReview] = useState(''); //state for modal review
-  const [revieww, setRevieww] = useState(''); //state for modal review
-  const [rate, setRate] = useState(''); //state for modal rate
-  const [showRate, setShowRate] = useState(false); // state for rate
-  const [showReview, setShowReview] = useState(false); //state for review
-  //const [rating, setRating] = useState(''); //state for rating
+  const [isOpenBReview, setIsOpenBReview] = useState(false); // state for review modal
 
-  const openModal = () => setIsOpen(true); // open modal
-  const closeModal = () => setIsOpen(false); // close modal
+  // open and close modal for review
+  const openModalBReview = () => setIsOpenBReview(true);
+  const closeModalBReview = () => setIsOpenBReview(false);
 
-  const toggleRate = () => setShowRate(!showRate); // toggle rate
-  const toggleReview = () => setShowReview(!showReview); // toggle review
-
-  // handle if dropbox choose rate or review to show
-  const handleReview = (e) => {
-    const value = e.target.value;
-    setReview(value);
-    setShowRate(value === 'rate');
-    setShowReview(value === 'review');
-};
-
-// handle review
-const handleSubmitReview = (e) => {
-  e.preventDefault();
-  console.log('Review:', revieww);
-  setRevieww('');
-}
-
-// handle rate
-const handleSubmitRate = (e) => {
-  e.preventDefault();
-  console.log('Rate:', rate);
-  setRate('');
-}
-
-  return(
-    <div className="p-4 min-h-screen w-3/4 overflow-x-auto">
-      <div className="m-2 max-w-sm rounded overflow-hidden shadow-lg">
-        <img className="w-full" src={agent1} alt="agent1"/>
-          <div className="px-6 py-4">
-             <div className="font-bold text-xl mb-2">Brian</div>
-                <p className="text-gray-700 text-base">
-                  Featured Condo Agents
-                  </p>
-            </div>
-            <button className="m-2 p-2 whitespace-nowrap border border-blue-400 rounded-md text-sm font-medium hover:border-blue-600 hover:text-blue-600" onClick={openModal}>
-               Rate/Review</button>
-               <Modal isOpen={isOpen} onRequestClose={closeModal} className="block p-2 w-1/2 mx-auto bg-gray-600">
-                <div className='flex p-3 mb-5 border-b-4 justify-evenly align-middle text-white'>Select Changes</div> {/* header*/}
-                <div className="mt-4 mb-3">
-                  <label htmlFor="roles" className="block mb-4 text-sm font-medium text-white">Select Changes</label>
-                  <select id="roles" value={review} onChange={handleReview} className="mb-3 block w-full px-3 py-2 border rounded-md shadow-sm hover:cursor-pointer focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                    <option value="" disabled>Select changes</option>
-                    <option value="review" onClick={toggleReview}>Review</option>
-                    <option value="rate" onClick={toggleRate}>Rate</option>
-                    </select>
-                    {/* show review */}
-                    {showReview && (
-                    <form onSubmit={handleSubmitReview}>
-                      <div className="my-4 border-b-2">
-                        <textarea id="Review" placeholder='Review' className="my-2 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required />
-                        </div>
-                        </form>
-                    )}
-                    {/* show rate */}
-                    {showRate && (
-                    <form onSubmit={handleSubmitRate}>
-                      <div className="my-4 border-b-2">
-                        <h1 id="Rate" className="text-2x1 text-white font-bold mt-2">Overall Rating</h1>
-                        <Rating />
-                        </div>
-                        </form>
-                    )}
-                  </div>
-                  <button className='p-3 mr-2 border border-white text-white text-sm rounded-md hover:cursor-pointer hover:bg-blue-300' onClick={closeModal}>Close</button>
-                  <button type='submit' className='p-3 mx-2 border border-white text-white text-sm rounded-md hover:cursor-pointer hover:bg-blue-300'>Send</button>
-                </Modal>
+  return (
+    <div className="min-h-screen w-3/4 overflow-x-auto">
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-50">
+          <tr>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              First Name
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Last Name
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Email
+            </th>
+            <th scope="col" className="relative px-6 py-3">
+              <span className="sr-only">Rate & Review</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {/* View Agent add key and mapping here */}
+          <tr>
+            <td className="px-6 py-4 whitespace-nowrap">Brian</td>
+            <td className="px-6 py-4 whitespace-nowrap">Low</td>
+            <td className="px-6 py-4 whitespace-nowrap">
+              realestatebrian@gmail.com
+            </td>
+            <button
+              className="m-2 p-4 whitespace-nowrap border border-blue-400 rounded-md text-sm font-medium hover:border-blue-600 hover:text-blue-600"
+              onClick={openModalBReview}
+            >
+              Rate & Review
+            </button>
+          </tr>
+        </tbody>
+      </table>
+      <Modal
+        isOpen={isOpenBReview}
+        onRequestClose={closeModalBReview}
+        className="block p-2 w-1/2 mx-auto bg-gray-600"
+      >
+        {/* header for modal*/}
+        <div className="flex p-3 mb-5 border-b-4 justify-evenly align-middle text-white">
+          Rate & Review
         </div>
+        <div className="mt-4 mb-3">
+          <h1 id="Rate" className="text-2x1 text-white font-bold mt-2">
+            Rating
+          </h1>
+          <Rating />
+          <div className="my-3"></div>
+          <textarea
+            id="BuyerReview"
+            placeholder="Review"
+            className="my-2 block w-full px-3 py-2 h-32 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            required
+          />
+        </div>
+        <button
+          className="p-3 mr-2 border border-white text-white text-sm rounded-md hover:cursor-pointer hover:bg-blue-300"
+          onClick={closeModalBReview}
+        >
+          Close
+        </button>
+        <button
+          type="submit"
+          className="p-3 mx-2 border border-white text-white text-sm rounded-md hover:cursor-pointer hover:bg-blue-300"
+        >
+          Post
+        </button>{" "}
+        {/* add onclick to create into database */}
+      </Modal>
     </div>
-
   );
 };
 
 const viewRRBAgent = () => {
-  
-    return (
-      <div id='viewA' className='flex'>
-        <DashboardBuyer/>
-        <RateReviewAgents/>
-      </div>
-    );
-  };
+  return (
+    <div id="viewA" className="flex">
+      <DashboardBuyer />
+      <RateReviewAgents />
+    </div>
+  );
+};
 export default viewRRBAgent;
